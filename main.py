25b4e2c6f2bca067852f1eca4a08c6ef4dba9041
Brian James Celine <brianceline@gmail.com>
2021-02-20 13:18:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+# List items are enclosed in square [] brackets
+# Lists are ordered
+# Lists are mutable and can be changed
+# List elements do not need to be unique
+# Lists can be of different data types
+
+# list = []
+# list = [1,2,3]
+# list = ['orange', 'apple', 'pear', 'apple', 'banana']
+# list = [1, 'Hello', 5.0]
+
+# print(list)
+
+'''
+List Indexing
+'''
+
+# fruits = ['orange', 'apple', 'pear', 'apple', 'banana']
+# print(fruits[0])
+
+# fruits = ['orange', 'apple', ['pear', 'apple', 'banana']]
+# print(fruits[2][1])
+
+'''
+How to Slice Lists in Python
+'''
+
+
+
+
+
+'''
+Add Elements to a List
+'''
+
+'''
+Remove or Delete List Items
+'''
+
+'''
+Python List Methods
+'''
