f3f98d0b4bbd6c0a18797c24444ea1ee89dd734f
Brian James Celine <brianceline@gmail.com>
2021-02-22 21:15:46 +0000

    Updated

--- main.py
+++ main.py
@@ -25,18 +25,103 @@ List Indexing
 How to Slice Lists in Python
 '''
 
+# fruits = ['orange', 'apple', 'pear', 'apple', 'banana']
 
-
-
+# print(fruits[:])
+# print(fruits[2:5])
+# print(fruits[:-3])
+# print(fruits[:3])
+# print(fruits[2:])
+# print(fruits[::2])
+# print(fruits[::-1])
 
 '''
 Add Elements to a List
 '''
 
+# fruits = ['orange', 'apple', 'pear', 'apple', 'banana']
+
+# fruits[0] = 'berries'
+# print(fruits)
+
+# fruits[1:4] = 'Mandarins', 'Peaches', 'Plums'
+# print(fruits)
+
+# fruits.append('Limes')
+# print(fruits)
+
+
 '''
 Remove or Delete List Items
 '''
 
+# fruits = ['orange', 'apple', 'pear', 'apple', 'banana']
+
+# del fruits[0]
+# print(fruits)
+# del fruits[1:5]
+# print(fruits)
+# del fruits
+# print(fruits)
+
+
 '''
 Python List Methods
 '''
+
+# print(dir(list))
+# print(help(list.index))
+
+# fruits = ['orange', 'apple', 'pear', 'apple', 'banana']
+# fruits.append('cashew')
+# print(fruits)
+
+# ==================================
+
+# fruits = ['orange', 'apple', 'pear', 'apple', 'banana']
+# fruits.insert(0, 'guava')
+# print(fruits)
+
+# ==================================
+
+# fruits = ['orange', 'apple', 'pear', 'apple', 'banana']
+# fruits.clear()
+# print(fruits)
+
+# ==================================
+
+# fruits = ['orange', 'apple', 'pear', 'apple', 'banana']
+# # fruits.pop(1)
+
+# pos = fruits.index('banana')
+# fruits.pop(pos)
+
+# print(fruits)
+
+# ==================================
+
+fruits = ['orange', 'apple', 'pear', 'apple', 'banana', 'banana', 'banana'] 
+
+# print(fruits.count('banana'))
+
+# result = {}
+# for x in fruits:
+#     result[x] = fruits.count(x)
+
+# print(result)
+
+# ==================================
+
+# from collections import Counter
+
+# print(Counter(fruits))
+
+# ==================================
+
+# '''
+# List Membership Test
+# '''
+
+# fruits = ['orange', 'apple', 'pear', 'banana'] 
+
+# print("apple" not in fruits)
